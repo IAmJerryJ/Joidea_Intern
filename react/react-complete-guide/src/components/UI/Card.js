e8fc1e536eaeaed701278dd7728d22fe11b205ac
IAmJerryJ <jerryjia1999429@gmail.com>
2022-12-21 18:52:42 +1100

    Update react

--- react/react-complete-guide/src/components/UI/Card.js
+++ react/react-complete-guide/src/components/UI/Card.js
@@ -1,6 +1,7 @@
 import "./Card.css";
 
-const Card = (props) =>{
+const Card = (props) => {
+  // To make the element has both itself class and 'Card' class
   const classes = "card " + props.className;
   return <div className={classes}>{props.children}</div>;
 }
