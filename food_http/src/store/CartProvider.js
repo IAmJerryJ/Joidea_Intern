a38d7dc77b8e079ca191ffa1ced6891b9032298e
IAmJerryJ <jerryjia1999429@gmail.com>
2023-01-18 10:11:02 +1100

    Redux counter

--- food_http/src/store/CartProvider.js
+++ food_http/src/store/CartProvider.js
@@ -34,7 +34,7 @@ const cartReducer = (state, action) => {
     // );
 
     // return { items, totalAmount };
-  
+
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
 
@@ -82,6 +82,10 @@ const cartReducer = (state, action) => {
     };
   }
 
+  if (action.type === "CLEAR") {
+    return defaultCartState;
+  }
+
   return defaultCartState;
 };
 
@@ -99,11 +103,16 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: "REMOVE", id: id });
   };
 
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: "CLEAR" });
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   };
 
   return (
---
+++ redux_project/src/components/Counter.js
@@ -0,0 +1,38 @@
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
+
+const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
+  const dispatch = useDispatch();
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggleCounter" });
+  };
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const decremnetHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5 });
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={decremnetHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
