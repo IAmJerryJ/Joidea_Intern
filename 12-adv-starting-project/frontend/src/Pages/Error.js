e83feea4c1555123e7f03aaf0ef171836b824582
IAmJerryJ <jerryjia1999429@gmail.com>
2023-01-22 18:45:15 +1100

    Add new projects

---
+++ 12-adv-starting-project/frontend/src/Pages/Error.js
@@ -0,0 +1,28 @@
+import MainNavigation from "../components/MainNavigation";
+import { useRouteError } from "react-router-dom";
+import PageContent from "../components/PageContent";
+
+function ErrorPage() {
+  const error = useRouteError();
+  let title = "Error occured";
+  let message = "Something went wrong";
+
+  if (error.status === 500) {
+      message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 404) {
+    title = "Not found";
+    message = "Could not find this page";
+  }
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
+}
+
+export default ErrorPage;
